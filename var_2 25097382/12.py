f2f4f7efaa8501e92a9ad86a2515d1096e835aec
Kaine <ivanovskija36@gmail.com>
2025-05-17 07:44:46 +0300

    12 добавил

---
+++ var_2 25097382/12.py
@@ -0,0 +1,15 @@
+for x in range(4, 10000):
+    print(x)
+    s = '1' + '9' * x
+    while '19' in s or '399' in s or '999' in s:
+        if '19' in s:
+            s = s.replace('19', '9', 1)
+        elif '399' in s:
+            s = s.replace('399', '91', 1)
+        elif '999' in s:
+            s = s.replace('999', '3', 1)
+    summary = sum([int(i) for i in s])
+    if summary == 33:
+        print(f'\n{x}\n{s}')  # Невозможно получить 33
+        break
+
